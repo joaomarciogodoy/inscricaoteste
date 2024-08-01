449f0915cd5cd76a3c96b975c8fc6876a524a6c3
João Márcio <joaomarciogodoy@gmail.com>
2024-07-31 23:23:35 -0300

    painelquempagou

--- models/pessoa.model.js
+++ models/pessoa.model.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose')
 
+const data_do_pagamento = Date.now()
+const dt_pagamento = new Date(data_do_pagamento)
+
+const pagamento_db = dt_pagamento.toLocaleDateString('pt-BR')
+
+
 module.exports = mongoose.model('Pessoa', {
     nome: String,
     email: String,
     idade: String,
     telefone: Number,
     cidade: String,
-    data_inscricao: { type: Date, default: Date.now() },
-    pagamento: { type: String, default: "Não" },
+    data_inscricao: { type: String, default: pagamento_db },
+    pagamento: { type: String, default: "NÃO" },
     data_pagamento: { type: String, default: null }
 })
